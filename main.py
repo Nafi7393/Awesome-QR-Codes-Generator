2f17e13ff8ef2b0760c3b72d916015a46dcdcb16
Nafi Ahmed <58775271+Nafi7393@users.noreply.github.com>
2022-12-12 22:28:17 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import random
-import string
 from itertools import cycle
-from PIL import Image
-from amzqr import amzqr
 import os
+import io
+import segno
 import streamlit as st
 from streamlit.components.v1 import html
 
@@ -50,70 +48,32 @@ st.markdown("""# Make Awesome QR Codes!""")
 st.markdown(basic_style, unsafe_allow_html=True)
 
 
-def remove_files(max_limit):
-    l_files = os.listdir("logo")
-    if len(l_files) >= max_limit:
-        for f in l_files:
-            os.remove(f"logo/{f}")
+@st.cache(suppress_st_warning=True, max_entries=20, allow_output_mutation=True)
+def making_qr_code(data, img):
 
-        o_files = os.listdir("output")
-        for i in o_files:
-            os.remove(f"output/{i}")
-
-
-def convert_img(pic_src):
-    base_width = 400
-    base_img = Image.open(pic_src).convert("RGB")
-    ratio = (base_width / float(base_img.size[0]))
-    height_size = int((float(base_img.size[1]) * float(ratio)))
-    img = base_img.resize((base_width, height_size), Image.Resampling.LANCZOS)
-    os.remove(pic_src)
-    img.save(f"{pic_src.split('.')[0]}.png", quality=95)
-
-
-def save_uploaded_file(uploadedfile):
-    with open(os.path.join("logo", uploadedfile.name), "wb") as f:
-        f.write(uploadedfile.getbuffer())
-
-
-@st.cache(suppress_st_warning=True, max_entries=20)
-def making_qr_code(qr_ink, img):
     if img:
-        save_uploaded_file(img)
-        name = img.name
-        src_pic_name = f"logo\\{img.name}"
-        if src_pic_name.split('.')[-1] == "png" or src_pic_name.split('.')[-1] == "gif":
-            pass
+        if img.name[-3:] == "gif":
+            kind = "gif"
         else:
-            convert_img(src_pic_name)
-            src_pic_name = f"{src_pic_name.split('.')[0]}.png"
-            name = src_pic_name.split('\\')[1]
+            kind = "png"
+
+        qrcode = segno.make(data, error='h')
+        out = io.BytesIO()
+        qrcode.to_artistic(background=img, target=out, scale=12, kind=kind)
+
+        return out
     else:
-        name = ''.join(random.choice(string.ascii_letters + "0123456789") for i in range(8)) + ".png"
-        src_pic_name = None
-
-    version, level, qr_name = amzqr.run(
-        words=qr_ink,
-        version=1,
-        level='H',
-        picture=src_pic_name,
-        colorized=True,
-        contrast=1.0,
-        brightness=1.0,
-        save_name=f"output\\{name}",
-        save_dir=os.getcwd())
-
-    return f'output/{name}'
+        qrcode = segno.make(data, error='h')
+        img = qrcode.to_pil(scale=12)
+        return img
 
 
 url_data = st.text_input("Input your data here.")
 image_file = st.file_uploader("Upload your image", type=['png', 'jpg', "jpeg", "gif", "webp"], label_visibility="collapsed")
 
 if st.button("Generate Now", type="primary"):
-    file= making_qr_code(url_data, image_file)
-    st.image(file, width=400)
-
-remove_files(max_limit=10)
+    file = making_qr_code(url_data, image_file)
+    st.image(file)
 
 
 html("""<h2 class='ex'><u> </u></h2>""")
